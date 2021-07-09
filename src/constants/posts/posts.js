f908214ae718919adbbae611efbe51974f9bd165
cassiorsfreitas <cassio.rsfreitas@gmail.com>
2021-07-09 23:30:23 +0100

    feat: added static posts in constants folder and updated nodemodules

---
+++ src/constants/posts/posts.js
@@ -0,0 +1,28 @@
+import loremImage from '../../../public/loremImage.jpg'
+import loremImage2 from '../../../public/loremImage2.jpg'
+
+const posts = [
+  {
+    cover: loremImage,
+    time: '3 min',
+    date: '17/06/2021',
+    title: 'Why should we know SEO?',
+    description:
+      'Tempor ad laboris deserunt ut ea. Cupidatat est eu ipsum culpa amet Lorem voluptate pariatur incididunt incididunt cillum amet ad.',
+    content:
+      'Excepteur eiusmod et id in nisi adipisicing. Proident magna cupidatat ut ea adipisicing. Magna aute adipisicing sint aute exercitation proident Lorem esse in sunt nostrud exercitation consectetur ullamco. Aliqua velit ut quis fugiat nostrud. Exercitation enim est commodo nisi laboris sint amet adipisicing incididunt mollit commodo Lorem non eiusmod. Incididunt ullamco officia eiusmod exercitation occaecat consequat eu nulla mollit commodo. Dolore quis qui enim consequat voluptate velit eu cillum in consequat reprehenderit excepteur sint excepteur. Esse duis incididunt qui id. Ex aliquip ullamco velit fugiat aliquip mollit quis nulla quis qui anim id. Veniam nulla irure sunt est amet reprehenderit nostrud excepteur ad.'
+  },
+
+  {
+    cover: loremImage2,
+    time: '3 min',
+    date: '17/06/2021',
+    title: 'Why should we know SEO? 2',
+    description:
+      'Tempor ad laboris deserunt ut ea. Cupidatat est eu ipsum culpa amet Lorem voluptate pariatur incididunt incididunt cillum amet ad.',
+    content:
+      'Excepteur eiusmod et id in nisi adipisicing. Proident magna cupidatat ut ea adipisicing. Magna aute adipisicing sint aute exercitation proident Lorem esse in sunt nostrud exercitation consectetur ullamco. Aliqua velit ut quis fugiat nostrud. Exercitation enim est commodo nisi laboris sint amet adipisicing incididunt mollit commodo Lorem non eiusmod. Incididunt ullamco officia eiusmod exercitation occaecat consequat eu nulla mollit commodo. Dolore quis qui enim consequat voluptate velit eu cillum in consequat reprehenderit excepteur sint excepteur. Esse duis incididunt qui id. Ex aliquip ullamco velit fugiat aliquip mollit quis nulla quis qui anim id. Veniam nulla irure sunt est amet reprehenderit nostrud excepteur ad.'
+  }
+]
+
+export default posts
