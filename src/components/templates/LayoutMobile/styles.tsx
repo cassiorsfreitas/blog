4606809b9e582a54dc66b3fd4a98e4f8272c36a1
cassiorsfreitas <cassio.rsfreitas@gmail.com>
2021-07-09 13:27:18 +0100

    fix: react version detect in eslint config

--- src/components/templates/LayoutMobile/styles.tsx
+++ src/components/templates/LayoutMobile/styles.tsx
@@ -1,7 +1,7 @@
 import styled, { css } from 'styled-components'
 
 export const Container = styled.div`
-  ${({ theme }) => css`
+  ${() => css`
     display: flex;
     height: 100%;
     width: 100%;
