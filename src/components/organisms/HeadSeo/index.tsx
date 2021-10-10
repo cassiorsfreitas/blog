e23fbb66a42475ee3fd3f72af27c69b921acebf6
Cássio Freitas <cassiofreitas@Cassios-MBP.Home>
2021-10-10 22:41:36 +0100

    fix: removed itemProp from metadata

--- src/components/organisms/HeadSeo/index.tsx
+++ src/components/organisms/HeadSeo/index.tsx
@@ -12,29 +12,21 @@ const HeadSeo = ({
   return (
     <Head>
       <title>{title}</title>
-      <meta name="author" content="Cássio Freitas" />
-      <meta name="description" content={description} />
-      <meta name="keywords" content={keyword} />
-      {/* <link rel="icon" href="/favicon.ico" /> */}
+      <meta name="author" content="Cássio Freitas"></meta>
+      <meta name="description" content={description}></meta>
+      <meta name="keywords" content={keyword}></meta>
 
-      {/* <!-- Google / Search Engine Tags --> */}
-      <meta itemProp="name" content="Cássio Freitas - Web Developer" />
-      <meta itemProp="description" content={description} />
-      <meta itemProp="image" content={cover} />
+      <meta property="og:type" content="website"></meta>
+      <meta property="og:url" content={slug}></meta>
+      <meta property="og:title" content={title}></meta>
+      <meta property="og:description" content={description}></meta>
+      <meta property="og:image" content={cover}></meta>
 
-      {/* <!-- Open Graph / Facebook --> */}
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content={slug} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:image" itemProp="image" content={cover} />
-
-      {/* <!-- Twitter --> */}
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content={slug} />
-      <meta property="twitter:title" content={title} />
-      <meta property="twitter:description" content={description} />
-      <meta property="twitter:image" itemProp="image" content={cover} />
+      <meta property="twitter:card" content="summary_large_image"></meta>
+      <meta property="twitter:url" content={slug}></meta>
+      <meta property="twitter:title" content={title}></meta>
+      <meta property="twitter:description" content={description}></meta>
+      <meta property="twitter:image" content={cover}></meta>
     </Head>
   )
 }
