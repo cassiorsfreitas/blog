979c07e1d838a4f28cc54929a51ada86e15c9dbf
cassiorsfreitas <cassio.rsfreitas@gmail.com>
2022-05-29 19:22:19 +0100

    chore: add husky and lintstaged

---
+++ jest-unit-config.js
@@ -0,0 +1,3 @@
+const config = require('./jest.config')
+config.testMatch = ['**/*.spec.tsx']
+module.exports = config
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
 module.exports = {
   setupFilesAfterEnv: ["./jest.setup.js"],
+  roots: ['<rootDir>/test', '<rootDir>/src'],
 };
