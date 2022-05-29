8cdc9d4f2dc90fe20457ef96729520c8ba3149fe
cassiorsfreitas <cassio.rsfreitas@gmail.com>
2022-05-29 20:50:24 +0100

    chore: add ignore path coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   setupFilesAfterEnv: ["./jest.setup.js"],
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}',
+    '!**/interface.{ts,tsx}',
+    '!**/styles.{ts,tsx}',
+    '!**/styles/**',
+    '!**/_app.tsx',
+    '!**/_document.tsx'
+  ],
   roots: ['<rootDir>/test', '<rootDir>/src'],
 };
