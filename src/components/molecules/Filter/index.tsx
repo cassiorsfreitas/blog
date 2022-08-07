cccfafca058170ccb74d0498f24635df37a3cfe1
cassiorsfreitas <cassio.rsfreitas@gmail.com>
2022-08-07 13:26:01 +0100

    refactor: create generateFilters out Filter component

--- src/components/molecules/Filter/index.tsx
+++ src/components/molecules/Filter/index.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
 import { PostInterface } from '../../../interfaces/postInterface'
+import { generateFilters } from '../../../lib/generateFilters'
 import Button from '../../atoms/Button'
 import SectionTitle from '../../atoms/SectionTitle'
 import { Container } from './styles'
@@ -13,40 +14,9 @@ const Filter = ({ posts }: Props) => {
   const [filters, setFilters] = useState<string[]>([])
   console.log('posts', posts)
 
-  const generateFilters = () => {
-    const filtersArray = []
-    // eslint-disable-next-line array-callback-return
-    posts.map(post => {
-      if (
-        post.metadata.category.includes('Front end') &&
-        !filtersArray.includes('front-end')
-      ) {
-        filtersArray.push('front-end')
-      }
-      if (
-        post.metadata.category.includes('SEO') &&
-        !filtersArray.includes('seo')
-      ) {
-        filtersArray.push('seo')
-      }
-      if (
-        post.metadata.category.includes('DevOps') &&
-        !filtersArray.includes('devops')
-      ) {
-        filtersArray.push('devops')
-      }
-      if (
-        post.metadata.category.includes('Quick tips') &&
-        !filtersArray.includes('quick-tips')
-      ) {
-        filtersArray.push('quick-tips')
-      }
-    })
-    setFilters(filtersArray)
-  }
-
   useEffect(() => {
-    generateFilters()
+    const filtersArray = generateFilters(posts)
+    setFilters(filtersArray)
   }, [])
 
   return (
