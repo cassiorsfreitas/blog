1d29a7f91f247a19317e9da523f3cbfbfe7cc382
cassiorsfreitas <cassio.rsfreitas@gmail.com>
2022-10-24 23:22:35 +0100

    test: button component should calls correct onclick function

--- src/components/atoms/Button/index.spec.tsx
+++ src/components/atoms/Button/index.spec.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
-import { render, screen } from '../../../../test/config/react-library'
+import {
+  render,
+  screen,
+  userEvent
+} from '../../../../test/config/react-library'
 import { ThemeProvider, dark } from '../../../../test/config/theme-wrapper'
 import Button from '.'
+import { ButtonProps } from './interface'
 
-const makeSut = () => {
+const makeSut = (props: Partial<ButtonProps> = {}) => {
+  const defaultProps: ButtonProps = {
+    onClick: jest.fn()
+  }
   return render(
     <ThemeProvider theme={dark}>
-      <Button>Any</Button>
+      <Button {...defaultProps} {...props}>
+        Any
+      </Button>
     </ThemeProvider>
   )
 }
@@ -16,5 +26,15 @@ describe('Button component', () => {
     makeSut()
     const button = screen.queryByTestId('button')
     expect(button).toBeInTheDocument()
+  })
+
+  it('should call correct onClick function', async () => {
+    const customOnClick = jest.fn()
+    makeSut({
+      onClick: customOnClick
+    })
+    const button = screen.getByTestId('button')
+    await userEvent.click(button)
+    expect(customOnClick).toHaveBeenCalled()
   })
 })
