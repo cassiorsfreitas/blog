2d49ff9e6363e9637466d423080d2d7bec7f714d
cassiorsfreitas <cassio.rsfreitas@gmail.com>
2022-11-19 20:43:03 +0000

    test: SectionTitle should render by default

--- src/components/atoms/SectionTitle/index.tsx
+++ src/components/atoms/SectionTitle/index.tsx
@@ -1,8 +1,12 @@
 import { Container } from './styles'
 import { SectionTitleProps } from './interface'
 
-const SectionTitle = ({ title, strong }: SectionTitleProps) => {
-  return <Container strong={strong}>{title}</Container>
+const SectionTitle = ({ title, strong = false }: SectionTitleProps) => {
+  return (
+    <Container strong={strong} data-testId="section-title">
+      {title}
+    </Container>
+  )
 }
 
 export default SectionTitle
