538f62850a1fa499d91a24b0b6135ca7720caae6
cassiorsfreitas <cassio.rsfreitas@gmail.com>
2022-11-26 23:10:48 +0000

    chore: add test script in the lint staged config file

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -4,5 +4,7 @@ module.exports = {
   '**/*.(ts|tsx|js)': filenames => [
     `yarn eslint --fix ${filenames.join(' ')}`,
     `yarn prettier --write ${filenames.join(' ')}`
-  ]
+  ],
+
+  '**/*.(ts|tsx)': () => 'yarn test:staged'
 }
