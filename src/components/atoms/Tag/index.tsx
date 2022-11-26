7d0f0e016198c1cb2f297be888df05d447fc0dac
cassiorsfreitas <cassio.rsfreitas@gmail.com>
2022-11-26 00:03:19 +0000

    refactor: update tag interface name

--- src/components/atoms/Tag/index.tsx
+++ src/components/atoms/Tag/index.tsx
@@ -1,6 +1,10 @@
 import { Container } from './styles'
 
-const Tag = ({ category }) => {
+export interface PropsTag {
+  category: string
+}
+
+const Tag = ({ category }: PropsTag) => {
   return <Container>{category}</Container>
 }
 
