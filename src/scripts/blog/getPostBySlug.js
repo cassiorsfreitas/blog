a5937fe62048d908efc9c7ba4748efab539747bf
cassiorsfreitas <cassio.rsfreitas@gmail.com>
2022-11-27 00:57:34 +0000

    fix: add function to verify if any post is a table content

--- src/scripts/blog/getPostBySlug.js
+++ src/scripts/blog/getPostBySlug.js
@@ -3,11 +3,16 @@ import matter from 'gray-matter'
 import remark from 'remark'
 import remarkHTML from 'remark-html'
 
+const isTableContent = slug => {
+  return slug === 'vim-cheat-sheet'
+}
+
 export function getPostBySlug(slug) {
   const markdownWithMeta = fs.readFileSync(`./_posts/${slug}.md`, 'utf-8')
   const { data: metadata, content } = matter(markdownWithMeta)
-  const htmlContent = remark().use(remarkHTML).processSync(content).toString()
-
+  const htmlContent = isTableContent(slug)
+    ? remark().processSync(content).toString()
+    : remark().use(remarkHTML).processSync(content).toString()
   return {
     metadata: {
       ...metadata
